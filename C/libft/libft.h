074e12b3b0edaed79a4a050f3ed21848c4fcd173
Adrian Galisteo <agaliste@Adrians-MacBook-Pro.local>
2021-01-22 15:37:06 +0100

    Added functions

---
+++ C/libft/libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaliste <agaliste@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/22 13:12:25 by agaliste          #+#    #+#             */
+/*   Updated: 2021/01/22 13:41:19 by agaliste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_isacii(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+void	ft_bzero(void *sr, size_t n);
+
+#endif
