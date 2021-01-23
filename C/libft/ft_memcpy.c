4808ac67050bf9c05f74f56ada51cb8e95576b15
Adrian Galisteo mula <agaliste@c2r4s5.42madrid.com>
2021-01-23 17:36:27 +0100

    Added function + updated header

---
+++ C/libft/ft_memcpy.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/23 16:40:44 by agaliste          #+#    #+#             */
+/*   Updated: 2021/01/23 17:35:05 by agaliste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (dst == NULL || src == NULL)
+		return (NULL);
+	while (i < n)
+	{
+		((char *)dst)[i] = ((const char *)src)[i];
+		i++;
+	}
+	return (dst);
+}
+
+int main(void)
+{
+	char *test = NULL;
+	char dest[] = "Adios";
+	
+	printf("%s", ft_memcpy(dest, test, strlen(test)+1));
+	return (0);
+}
--- C/libft/libft.h
+++ C/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 13:12:25 by agaliste          #+#    #+#             */
-/*   Updated: 2021/01/22 22:44:59 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/01/23 17:12:52 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,7 @@ size_t	ft_strlen(const char *str);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 #endif
