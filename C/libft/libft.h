e53021ed76b34e0344291d14e335a0e7cb9cccea
Adrian Galisteo mula <agaliste@c2r17s2.42madrid.com>
2021-02-01 17:02:40 +0100

    Norme correction

--- C/libft/libft.h
+++ C/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/22 13:12:25 by agaliste          #+#    #+#             */
-/*   Updated: 2021/02/01 08:01:11 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/02/01 16:59:18 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,6 +84,6 @@ void				ft_lstdelone(t_list *lst, void (*del)(void*));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
 void				ft_lstiter(t_list *lst, void (*f)(void *));
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-						void (*del)(void *));	
+						void (*del)(void *));
 
 #endif
