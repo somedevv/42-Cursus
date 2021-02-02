1e0352ee4d1783dc91e2e553d0155198c451e2ee
Adrian Galisteo <agaliste@Adrians-MacBook-Pro.local>
2021-02-02 18:41:53 +0100

    Fixes for some functions according to pass libf-unit-test

--- C/libft/ft_strncmp.c
+++ C/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 13:36:09 by agaliste          #+#    #+#             */
-/*   Updated: 2021/01/25 16:43:09 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/02/01 20:32:31 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int i;
 
 	i = 0;
-	while (n--)
+	while (i < n && s1[i] != '\0' && s2[i] != '\0')
 	{
 		if ((unsigned char)s1[i] != (unsigned char)s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
+	if (i != n)
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 	return (0);
 }
