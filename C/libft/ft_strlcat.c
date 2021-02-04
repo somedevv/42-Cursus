6a9dc834465e883a590b0f9ee8aa568c5957ed33
Adrian Galisteo mula <agaliste@c2r17s1.42madrid.com>
2021-02-04 16:46:12 +0100

    Remade ft_strlcat to fix abort on lift-war-machine

--- C/libft/ft_strlcat.c
+++ C/libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 13:27:20 by agaliste          #+#    #+#             */
-/*   Updated: 2021/01/22 15:43:28 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/02/04 16:40:47 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,19 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int				src_size;
-	unsigned int	dest_size;
-	unsigned int	i;
+	size_t	d;
+	size_t	dst_size;
 
-	i = 0;
-	src_size = 0;
-	dest_size = 0;
-	while (src[src_size])
-		src_size++;
-	if (size == 0)
-		return (src_size);
-	while (dst[dest_size])
-		dest_size++;
-	if (size <= dest_size)
-		return (size + src_size);
-	i = 0;
-	while (size && (--size - dest_size) && src[i])
+	dst_size = ft_strlen(dst);
+	if (size <= ft_strlen(dst))
+		return (size + ft_strlen(src));
+	d = 0;
+	while (src[d] != '\0' && dst_size + 1 < size)
 	{
-		dst[dest_size + i] = src[i];
-		i++;
+		dst[dst_size] = src[d];
+		dst_size++;
+		d++;
 	}
-	dst[dest_size + i] = '\0';
-	return (src_size + dest_size);
+	dst[dst_size] = '\0';
+	return (ft_strlen(dst) + ft_strlen(&src[d]));
 }
