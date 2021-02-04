cf25e03ab6c8a8b07013a875402fb247dc0d4998
Adrian Galisteo <agaliste@Adrians-MacBook-Pro.local>
2021-02-04 09:56:42 +0100

    Added new project [get_next_line]: Completed get_next_line_utils + Added header + added main file

---
+++ C/get_next_line/get_next_line.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/04 09:03:47 by agaliste          #+#    #+#             */
+/*   Updated: 2021/02/04 09:51:22 by agaliste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <string.h>
+
+/*
+** Main Functions
+*/
+
+int		get_next_line(int fd, char **line);
+
+/*
+** Bonus Functions
+*/
+
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(const char *s1, const char *s2);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+int		check_return(char *str);
+
+#endif
---
+++ C/get_next_line/get_next_line_utils.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/04 09:09:13 by agaliste          #+#    #+#             */
+/*   Updated: 2021/02/04 09:45:09 by agaliste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char *d;
+	char *s;
+
+	d = (char *)dst;
+	s = (char *)src;
+	if (dst == src)
+		return (dst);
+	if (s < d)
+	{
+		while (len--)
+			*(d + len) = *(s + len);
+		return (dst);
+	}
+	while (len--)
+		*d++ = *s++;
+	return (dst);
+}
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char			*str;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	if (!(str = malloc((sizeof(char) * ft_strlen(s1) + ft_strlen(s2)) + 1)))
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		str[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		str[j++] = s2[i++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	return (str);
+}
+
+int		check_return(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str)
+	{
+		while (str[i])
+		{
+			if (str[i] == '\n')
+				return (1);
+			i++;
+		}
+	}
+	return (0);
+}
