85546e8cc0b63f45a46a1b5dc6b7fdc859413335
Adrian Galisteo mula <agaliste@c1r14s1.42madrid.com>
2021-03-09 18:41:03 +0100

    Idk :(

--- C/get_next_line/get_next_line.c
+++ C/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 09:03:22 by agaliste          #+#    #+#             */
-/*   Updated: 2021/03/08 19:17:16 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/03/09 17:50:57 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,44 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int get_next_line(int fd, char **line) {
-	static char buff[BUFFER_SIZE + 1];
+int	get_next_line(int fd, char **line)
+{
+	static char	buff[BUFFER_SIZE + 1];
 	int			rd;
 
-	while ((rd = read(fd, &buff,BUFFER_SIZE)) >= 0)
+	while ((rd = read(fd, buff, BUFFER_SIZE)) >= 0)
 	{
-		buff[rd] = 0;
+		buff[rd] = '\0';
 		ft_strjoin(*line, buff);
-		ft_bzero(ft_strchr(*line, '\n'), ft_strlen(ft_strchr(*line, '\n')));
-		//return (1);
+		// ft_bzero(ft_strchr(*line, '\n'), ft_strlen(ft_strchr(*line, '\n')));
 	}
 	if (rd == -1)
-		return(-1);
+		return (-1);
 	else
 		return (0);
 }
 
-int	main() {
-	int fd = open("./test.txt", O_RDONLY);
-	char **line;
+int	main(void)
+{
+	int		fd;
+	char	*line;
 
-	if (!(line = malloc(sizeof(char **))))
-		return (-1);
-	get_next_line(fd, line);
-   	for(int i=0; i<2; i++) {
-      	for(int j=0;j<3;j++) {
-        	 printf("%d ", line[i][j]);
-        	 if(j==2){
-            	printf("\n");
-        	 }
-      	}
-   	}
-   return 0;
+	line = malloc(sizeof(char*));
+	fd = open("/Users/agaliste/42-Cursus/C/get_next_line/test.txt", O_RDONLY);
+	while (get_next_line(fd, &line))
+	{
+		printf("Line: %s", line);
+	}
+	/*
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d ", line[i][j]);
+			if (j == 2)
+				printf("\n");
+		}
+	}
+	*/
+	return (0);
 }
