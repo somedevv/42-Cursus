dd78275f278973ab0cc4e472ea9466035561ea80
somedevv <adriancrmini@gmail.com>
2021-03-15 17:56:23 +0100

    GNL Finished! :D

--- C/get_next_line/get_next_line.c
+++ C/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 09:03:22 by agaliste          #+#    #+#             */
-/*   Updated: 2021/03/11 13:34:49 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/03/15 17:17:17 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,100 @@
 #include <stdio.h>
 #include <fcntl.h>
 
+char	*cpy_str(char *pos, char **line, int rd)
+{
+	int		i;
+	char	*help;
+
+	i = 0;
+	while (pos[i])
+	{
+		if (pos[i] == '\n')
+			break ;
+		i++;
+	}
+	if (i < ft_strlen(pos))
+	{
+		*line = ft_substr(pos, 0, i);
+		help = ft_substr(pos, i + 1, ft_strlen(pos) + 1);
+		free(pos);
+		pos = ft_strdup(help);
+		free(help);
+	}
+	if (rd == 0)
+	{
+		*line = pos;
+		pos = NULL;
+	}
+	return (pos);
+}
+
+int	check_str(char *buff, char **pos)
+{
+	char	*help;
+
+	if (*pos)
+	{
+		help = ft_strjoin(*pos, buff);
+		free(*pos);
+		*pos = ft_strdup(help);
+		free(help);
+	}
+	else
+		*pos = ft_strdup(buff);
+	if (ft_strchr(buff, '\n'))
+		return (1);
+	return (0);
+}
+
+int	final_checks(char **line, char **pos, int fd, int rd)
+{
+	if (rd <= 0 && !pos[fd])
+	{
+		*line = ft_strdup("");
+		return (rd);
+	}
+	pos[fd] = cpy_str(pos[fd], line, rd);
+	if (rd <= 0 && !pos[fd])
+		return (rd);
+	return (1);
+}
+
 int	get_next_line(int fd, char **line)
 {
-	
+	char		*buff;
+	static char	*pos[256];
+	int			rd;
+
+	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (BUFFER_SIZE <= 0 || !fd || !line || !buff)
+		return (-1);
+	rd = read(fd, buff, BUFFER_SIZE);
+	while (rd)
+	{
+		if (rd == -1)
+		{
+			free(buff);
+			return (-1);
+		}
+		buff[rd] = '\0';
+		if (check_str(buff, &pos[fd]))
+			break ;
+		rd = read(fd, buff, BUFFER_SIZE);
+	}
+	free(buff);
+	return (final_checks(line, pos, fd, rd));
 }
 
 int	main(void) //Main for testing
 {
 	int		fd;
+	int		ret;
 	char	*line;
 
 	line = malloc(sizeof(char*));
-	fd = open("/Users/agaliste/42-Cursus/C/get_next_line/test.txt", O_RDONLY);
-	/*
-	while (get_next_line(fd, &line))
-		printf("Line: %s", line);
-	*/
+	fd = open("/Users/agaliste/Documents/Programming/42-Cursus/C/get_next_line/test.txt", O_RDONLY);
+	while((ret = get_next_line(fd, &line)) > 0)
+		printf("Line: %s\n", line);
 	return (0);
 }
--- C/get_next_line/get_next_line.h
+++ C/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 09:03:47 by agaliste          #+#    #+#             */
-/*   Updated: 2021/03/11 13:22:40 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/03/15 15:50:43 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int			get_next_line(int fd, char **line);
 size_t		ft_strlen(const char *s);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
-/*char		*ft_strdup(const char *s1);*/
+char		*ft_strdup(const char *s1);
 void		ft_bzero(void *s, size_t n);
 char		*ft_strchr(const char *s, int c);
 
