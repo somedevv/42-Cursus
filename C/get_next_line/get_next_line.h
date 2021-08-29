0ac52abda464551dff726e051118f1baffebc645
Adrian Galisteo mula <agaliste@c1r6s3.42madrid.com>
2021-08-29 04:40:57 +0200

    GNL: Updated header

--- C/get_next_line/get_next_line.h
+++ C/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/04 09:03:47 by agaliste          #+#    #+#             */
-/*   Updated: 2021/08/29 03:27:53 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/08/29 04:40:09 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,7 @@ char		*get_next_line(int fd);
 */
 
 size_t		ft_strlen(const char *s);
-char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
-char		*ft_strdup(const char *s1);
 void		ft_bzero(void *s, size_t n);
 char		*ft_strchr(const char *s, int c);
 void		*ft_memcpy(void *dst, const void *src, size_t n);
