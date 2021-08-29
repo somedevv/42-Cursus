010f99f6f3d91fa171dec2ca1293640265f5e21d
Adrian Galisteo mula <agaliste@c1r6s3.42madrid.com>
2021-08-30 01:21:03 +0200

    GNL: Fixed SEGFAULT

--- C/get_next_line/get_next_line.c
+++ C/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/13 23:37:11 by agaliste          #+#    #+#             */
-/*   Updated: 2021/08/29 16:14:47 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/08/30 00:10:14 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int	check_buffer(char *pos, char **line, int fd)
+int	check_buffer(char *pos, char **line)
 {
 	char	*aux;
 	char	*otro;
-	char	aux2[256][BUFFER_SIZE + 1];
+	char	aux2[BUFFER_SIZE + 1];
 
 	aux = ft_strchr(pos, '\n');
 	otro = *line;
@@ -27,10 +27,10 @@ int	check_buffer(char *pos, char **line, int fd)
 		*aux = '\0';
 		*line = ft_strjoin(otro, pos);
 		free(otro);
-		ft_memcpy(aux2[fd], aux + 1, ft_strlen(aux + 1));
+		ft_memcpy(aux2, aux + 1, ft_strlen(aux + 1));
 		ft_bzero(pos, BUFFER_SIZE + 1);
-		ft_memcpy(pos, aux2[fd], ft_strlen(aux2[fd]));
-		ft_bzero(aux2[fd], BUFFER_SIZE + 1);
+		ft_memcpy(pos, aux2, ft_strlen(aux2));
+		ft_bzero(aux2, BUFFER_SIZE + 1);
 		otro = *line;
 		*line = ft_strjoin(otro, "\n");
 		free(otro);
@@ -47,27 +47,26 @@ char	*get_next_line(int fd)
 	char		*line;
 	static char	pos[256][BUFFER_SIZE + 1];
 	int			rd;
-	int			i;
 
-	i = 0;
 	line = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (fd < 0 || fd > 256 || BUFFER_SIZE <= 0 || !line)
 		return (NULL);
 	ft_bzero(line, BUFFER_SIZE + 1);
-	if (check_buffer(pos[fd], &line, fd))
+	if (check_buffer(pos[fd], &line))
 		return (line);
 	rd = read(fd, pos[fd], BUFFER_SIZE);
 	while (rd)
 	{
-		i++;
-		if (check_buffer(pos[fd], &line, fd))
+		if (check_buffer(pos[fd], &line))
 			return (line);
 		rd = read(fd, pos[fd], BUFFER_SIZE);
 	}
-	if (line && i > 0)
-		return (line);
-	free(line);
-	return (NULL);
+	if (line[0] == '\0')
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
 }
 
 int	main(void)
@@ -75,8 +74,8 @@ int	main(void)
 	char	*linea;
 	int		fd;
 
-	fd = open("test.txt", O_RDONLY);
-	for (int i = 0; i < 6; i++)
+	fd = open("test", O_RDONLY);
+	for (int i = 1; i <= 2; i++)
 	{
 		linea = get_next_line(fd);
 		printf("Linea %d: %s\n", i, linea);
