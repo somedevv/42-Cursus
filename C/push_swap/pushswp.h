72428dd3c474ccf7e6af2215d0f2fd0cf2d344f5
somedevv <adriancrmini@gmail.com>
2021-10-21 19:23:41 +0200

    push_swap: Cleaned junk, refactor test code and made new Makefile

--- C/push_swap/pushswp.h
+++ C/push_swap/pushswp.h
@@ -21,9 +21,6 @@ typedef struct	s_stack
 ** Main Functions
 */
 
-char	push_swap(int argc, char **argv);
-void	pushswp(int argc, int *a, int *b);
-
 /*
 ** Operator Functions
 */
@@ -36,19 +33,8 @@ void	push(t_list	**src, t_list	**dst);
 ** Helper Functions
 */
 
-int		checkstring(char **argv, int argc);
-int		checkint(long long int nbr);
-int		checkrepeat(int *a, int argc);
-int		find_greater(int *arr, int argc);
-// int		moves_to_bottom(int *arr, int nbr, int argc);
-void	freestack(int *a, int *b);
-int		initchecks(char **argv, int argc, int *a, int *b);
-
 /*
 ** Solver Functions
 */
-
-void	solve2(t_list *lst);
-// void	solve_3(int *a);
 
 #endif
