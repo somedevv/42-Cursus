0301e623b415beddc084b0682926697e53281def
Adrian Galisteo mula <agaliste@c3r12s2.42madrid.com>
2021-10-25 15:57:43 +0200

    push_swap: Added work with new stack structure

--- C/push_swap/push_swap.c
+++ C/push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 15:14:57 by agaliste          #+#    #+#             */
-/*   Updated: 2021/10/22 21:30:20 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/10/23 03:57:11 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack *a;
+	t_list *a;
 	// t_list *b;
 
 	a = NULL;
 	init(argv, argc, a);
+	ft_lstclear(&a, free);
+	system("leaks push_swap");
 	return 0;
 }
