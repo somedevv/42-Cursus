cef0179d9c62e5f4fd1f7a22196ba6d314a2deff
somedevv <adriancrmini@gmail.com>
2021-11-05 18:13:59 +0100

    so_long: Argument parse done & libft: updated strnstr to protect str

--- Rank 2/so_long/helper/tools.c
+++ Rank 2/so_long/helper/tools.c
@@ -6,27 +6,29 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 02:18:36 by agaliste          #+#    #+#             */
-/*   Updated: 2021/11/05 02:40:39 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/11/05 18:07:30 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../solong.h"
 
-void	parseinput(int argc, char *argv)
+void printerror(char *error)
 {
-	char *papa;
+	write(1, "Error\n", 7);
+	write(1, error, ft_strlen(error));
+	exit(1);
+}
+
+void	parseinput(const int argc, const char *argv)
+{
+	char *tmp;
 	if (argc > 2)
-	{
-		write(1, "Error\n", 7);
-		exit(1);
-	}
-	papa = ft_strnstr(argv, ".ber", ft_strlen(argv));
-	if (papa)
-	{
-		if(ft_strlen(papa) > 4)
-		{
-			write(1, "Error\n", 7);
-			exit(1);
-		}
-	}
+		printerror("Only one argument is valid\n");
+	if (argc < 2)
+		printerror("Please provide a .ber map file as argument\n");
+	tmp = ft_strnstr(ft_strrchr(tmp, '.'), ".ber", 4);
+	if (tmp && ft_strlen(tmp) == 4)
+		return ;
+	else
+		printerror("Only .ber map file accepted as argument\n");
 }
