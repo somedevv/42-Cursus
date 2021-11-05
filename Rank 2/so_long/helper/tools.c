a974394a871a2f1534707e45bc317f4a572a9765
somedevv <adriancrmini@gmail.com>
2021-11-05 20:03:52 +0100

    so_long: minor norme fixes

--- Rank 2/so_long/helper/tools.c
+++ Rank 2/so_long/helper/tools.c
@@ -6,13 +6,13 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 02:18:36 by agaliste          #+#    #+#             */
-/*   Updated: 2021/11/05 18:07:30 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/11/05 18:30:55 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../solong.h"
 
-void printerror(char *error)
+void	printerror(char *error)
 {
 	write(1, "Error\n", 7);
 	write(1, error, ft_strlen(error));
@@ -21,7 +21,8 @@ void printerror(char *error)
 
 void	parseinput(const int argc, const char *argv)
 {
-	char *tmp;
+	char	*tmp;
+
 	if (argc > 2)
 		printerror("Only one argument is valid\n");
 	if (argc < 2)
