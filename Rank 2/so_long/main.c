a513245fcf5c81688592a38b9dc47e6d25437444
somedevv <adriancrmini@gmail.com>
2021-11-07 06:14:46 +0100

    so_long: removed unused variable

--- Rank 2/so_long/main.c
+++ Rank 2/so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: agaliste <agaliste@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 01:48:37 by agaliste          #+#    #+#             */
-/*   Updated: 2021/11/07 06:04:43 by agaliste         ###   ########.fr       */
+/*   Updated: 2021/11/07 06:14:05 by agaliste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,12 @@ int	main(int argc, char **argv)
 {
 	t_data img;
 	int	fd;
-	char **map;
 
 	parseinput(argc, argv[1]);
 	fd = open(argv[1], O_RDONLY);
-	map = parsemap(fd);
-	printmap(map);
-	img.coin = 0;
+	img.map = parsemap(fd);
+	printmap(img.map);
 	img.mlx = mlx_init();
-	img.map = map;
 	img.win = mlx_new_window(img.mlx, 30 * ft_strlen(img.map[0]), 27 * getleny(img.map), "Best espanita chaval");
 	drawground(&img);
 	drawelse(img);
